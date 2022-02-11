0196de8e08ecafaeccbbc449859d213ee889fd9a
taku <takchiks@gmail.com>
2022-02-11 14:37:58 -0500

    map location cpu

--- adversary.py
+++ adversary.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
         # x_t = torch.Tensor(x)
         # log_string(x_t)
         y_t = torch.Tensor(y)
-        tensor = torch.t(x)
+        tensor = x
         new_tensor, orig_prediction, new_prediction = attack(
             tensor, net, eps=1e-3, n_iter=100
             )
