2ee8be2fdb220d6c171434f8bfa51bc9bb8e5a9a
taku <takchiks@gmail.com>
2022-02-11 12:15:13 -0500

    attack pointnet initial

--- adversary.py
+++ adversary.py
@@ -6,6 +6,35 @@ import torchvision.models as models
 
 from utils import compute_gradient, read_image, to_array
 
+from ModelNetDataLoader import ModelNetDataLoader
+import argparse
+import numpy as np
+import os
+import torch
+import logging
+from tqdm import tqdm
+import sys
+import importlib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = BASE_DIR
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+
+
+def parse_args():
+    '''PARAMETERS'''
+    parser = argparse.ArgumentParser('Testing')
+    parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
+    parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+    parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
+    parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
+    parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
+    parser.add_argument('--log_dir', type=str, default='pointnet2_cls_ssg', required=True, help='Experiment root')
+    parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
+    parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
+    parser.add_argument('--num_votes', type=int, default=3, help='Aggregate classification scores with voting')
+    return parser.parse_args()
+
 def func(inp, net=None, target=None):
 
     out = net(inp)
@@ -39,12 +68,59 @@ def attack(tensor, net, eps=1e-3, n_iter=50):
 
 
 if __name__ == "__main__":
-    net = models.resnet18(pretrained=True)
+
+    def log_string(str):
+        logger.info(str)
+        print(str)
+
+
+    args = parse_args()
+    '''HYPER PARAMETER'''
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    '''CREATE DIR'''
+    experiment_dir = '../../PointClouds/Pointnet_Pointnet2_pytorch/log/classification/' + args.log_dir
+
+    '''LOG'''
+    args = parse_args()
+    logger = logging.getLogger("Model")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler('%s/eval.txt' % experiment_dir)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    log_string('PARAMETER ...')
+    log_string(args)
+
+    '''DATA LOADING'''
+    log_string('Load dataset ...')
+    data_path = '../../PointClouds/Pointnet_Pointnet2_pytorch/data/modelnet40_normal_resampled/'
+
+    test_dataset = ModelNetDataLoader(root=data_path, args=args, split='test', process_data=False)
+    testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
+
+    '''MODEL LOADING'''
+    num_class = args.num_category
+    model_name = os.listdir(experiment_dir + '/logs')[0].split('.')[0]
+    model = importlib.import_module(model_name)
+
+    classifier = model.get_model(num_class, normal_channel=args.use_normals)
+    if not args.use_cpu:
+        classifier = classifier.cpu()
+
+    checkpoint = torch.load(str(experiment_dir) + '/checkpoints/best_model.pth')
+    classifier.load_state_dict(checkpoint['model_state_dict'])
+
+    # net = models.resnet18(pretrained=True)
+    # net.eval()
+    #
+    # tensor = read_image("img.jpg")
+    net = classifier
     net.eval()
-
-    tensor = read_image("img.jpg")
-
-    new_tensor, orig_prediction, new_prediction = attack(
+    for step, (x, y) in enumerate(test_dataset):
+        tensor = torch.cat((x, y), -1)
+        new_tensor, orig_prediction, new_prediction = attack(
             tensor, net, eps=1e-3, n_iter=100
             )
 
