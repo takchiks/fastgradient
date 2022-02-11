c8bedfce0f6bb1bf91a8dfe28d4e08adb0055ff9
taku <takchiks@gmail.com>
2022-02-11 12:39:17 -0500

    remove --log required 3

--- adversary.py
+++ adversary.py
@@ -29,7 +29,7 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
     parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
-    parser.add_argument('--log_dir', type=str, default='2021-12-04_23-50', help='Experiment root')
+    parser.add_argument('--log_dir', type=str, default='2021-12-04_19-30', help='Experiment root')
     parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
     parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
     parser.add_argument('--num_votes', type=int, default=3, help='Aggregate classification scores with voting')
