700ffa1e9fa721021c5fa5fa11ec196540ea3d3f
taku <takchiks@gmail.com>
2022-03-11 00:31:08 -0500

    save text point cloud

--- adversary.py
+++ adversary.py
@@ -73,7 +73,7 @@ def attack(tensor, net, step, eps=0.005, n_iter=5):
             break
     tensor_numpy = new_tensor.transpose(1, 2).detach().numpy()
     tensor_string = ""
-    data_path = 'examples'
+    data_path = '../../PointClouds/Pointnet_Pointnet2_pytorch/data/modelnet40_normal_resampled/'
 
     if parse_args().num_category == 10:
         catfile = os.path.join(data_path, 'modelnet10_shape_names.txt')
@@ -86,6 +86,7 @@ def attack(tensor, net, step, eps=0.005, n_iter=5):
     print(f"classes {classes}\n")
 
     print(f"cat {cat}\n")
+    data_path= "examples"
     for a in tensor_numpy:
         tensor_string = tensor_string + ','.join(map(str, a)) + "\n"
     filenaming = os. path. join(data_path,f"{cat[orig_prediction]}", f"{step}_output.txt")
