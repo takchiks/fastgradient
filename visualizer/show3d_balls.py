f0a5f1eedae6fc1fed129fad42f687fa62cc1560
Taku <takchiks@gmail.com>
2022-04-06 12:24:42 -0400

    visulizer color green

--- visualizer/show3d_balls.py
+++ visualizer/show3d_balls.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
     # data = np.loadtxt("../examples/airplane/0_output.txt", delimiter = "\n")
     point_list = []
     i = 0
-    with open('../examples/chair/chair_0966.txt', 'r') as data:
+    with open('../examples/guitar/guitar_0161.txt', 'r') as data:
         for line in data:
             point_list.append([])
             point_list[i] = [n for n in line.split(',')]
