1edc2e20aef95f3b5e4735b3dea6a8fe2f3813fc
Taku <takchiks@gmail.com>
2022-04-16 18:18:14 -0400

    visulizer epsilon

--- adversary.py
+++ adversary.py
@@ -222,8 +222,8 @@ if __name__ == "__main__":
     class_total = [0 for i in range(40)]
     class_countdiff = [0 for i in range(40)]
     real_adv = []
-    epsilon = 0.0
-    epochs = 0
+    epsilon = 0.01
+    epochs = 1
     logging_string = ""
 
     for step, (x, y) in tqdm(enumerate(testDataLoader), total=len(testDataLoader)):
