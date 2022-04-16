cc96af200643cddd469b77b77e06da792b4642af
Taku <takchiks@gmail.com>
2022-04-16 17:26:43 -0400

    visulizer epsilon

--- visualizer/show3d_balls.py
+++ visualizer/show3d_balls.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
     point_list = []
     i = 0
     # with open('../../../PointClouds/Pointnet_Pointnet2_pytorch/data/modelnet40_normal_resampled/guitar/guitar_0167.txt', 'r') as data:
-    with open('../examples/5e-05and10/guitar/guitar_0169.txt', 'r') as data:
+    with open('../examples/5e-05and10/guitar/guitar_0205.txt', 'r') as data:
         for line in data:
             point_list.append([])
             point_list[i] = [n for n in line.split(',')]
