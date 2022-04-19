abcc436ded047238206d54f254be086c707ca179
Taku <takchiks@gmail.com>
2022-04-18 20:36:53 -0400

    visulizer epsilon

--- adversary.py
+++ adversary.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
     if not args.use_cpu:
         classifier = classifier.cpu()
 
-    checkpoint = torch.jit.load(str(experiment_dir) + '/checkpoints/best_model.pth', map_location=torch.device('cpu'))
+    checkpoint = torch.load(str(experiment_dir) + '/checkpoints/best_model.pth', map_location=torch.device('cpu'))
     classifier.load_state_dict(checkpoint['model_state_dict'])
 
     # net = models.resnet18(pretrained=True)
