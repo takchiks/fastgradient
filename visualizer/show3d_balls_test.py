79f54337aaad958d4f2e62ce97b80b305e283c98
Taku <takchiks@gmail.com>
2022-04-22 16:17:07 -0400

    visulizer epsilon

--- visualizer/show3d_balls_test.py
+++ visualizer/show3d_balls_test.py
@@ -230,8 +230,8 @@ if __name__ == '__main__':
     i = 0
     # import ModelNetDataLoader
 
-    # with open('../../../PointClouds/Pointnet_Pointnet2_pytorch/data/modelnet40_normal_resampled/airplane/airplane_0723.txt', 'r') as data:
-    with open('../examples/0.01and3/lamp/lamp_0129_diff.txt', 'r') as data:
+    with open('../../../PointClouds/Pointnet_Pointnet2_pytorch/data/modelnet40_normal_resampled/lamp/lamp_0129.txt', 'r') as data:
+    # with open('../examples/0.01and3/lamp/lamp_0129_diff.txt', 'r') as data:
         for line in data:
             point_list.append([])
             point_list[i] = [n for n in line.split(',')]
