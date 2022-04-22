213e518c546d34e38e054e13928f720d3a000d6d
Taku <takchiks@gmail.com>
2022-04-22 14:34:23 -0400

    visulizer epsilon

--- adversary.py
+++ adversary.py
@@ -223,12 +223,11 @@ if __name__ == "__main__":
     class_total = [0 for i in range(40)]
     class_countdiff = [0 for i in range(40)]
     real_adv = []
-    epsilon = 0.3
-    epochs = 1
+    epsilon = 0.01
+    epochs = 5
     logging_string = ""
 
     for step, (x, y) in tqdm(enumerate(testDataLoader), total=len(testDataLoader)):
-
         x = x.transpose(2, 1)
         # x_t = torch.Tensor(x)
         # log_string(x_t)
